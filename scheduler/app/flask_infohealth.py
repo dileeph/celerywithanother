c8369719431288e2a246e4b3b5e5e6bb559913ed
Dileep Hareendran <dileep.hareendran@gmail.com>
2021-11-08 17:23:17 +0530

    added logging

--- scheduler/app/flask_infohealth.py
+++ scheduler/app/flask_infohealth.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_restful import Resource, Api
+import logging
 
 app = Flask(__name__)
 api = Api(app)
 
 class Health(Resource):
     def get(self):
+        logging.debug("Hellooooooooooo")
         return {'hello': 'world'}
 
 class Info(Resource):
